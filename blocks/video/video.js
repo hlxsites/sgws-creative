d734e2c9551776031746e03035a082f203ecd75c
Tania Mathern <mathern@adobe.com>
2023-05-30 14:03:05 -0700

    Simplify

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,30 +1,30 @@
+import { createTag } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
   let posterImage = null;
 
-  block.querySelectorAll('img').forEach((image) => {
-    const imageHolderParagraph = image.closest('p');
-    imageHolderParagraph.remove();
-    posterImage = image.src;
-  });
+  // only one poster image (before or after the video link)
+  const image = block.querySelector('img');
+  const imageHolderParagraph = image.closest('p');
+  imageHolderParagraph.remove();
+  posterImage = image.src;
 
-  block.querySelectorAll('a').forEach((videoLink) => {
-    const divToReplace = videoLink.closest('div').parentNode;
-    const videoDiv = document.createElement('div');
+  // only one video link per block
+  const videoLink = block.querySelector('a');
+  block.textContent = '';
 
-    videoDiv.classList.add('video-content');
-    const videoElement = document.createElement('video');
-    videoElement.innerHTML = `<source src="${videoLink.href}" type="video/mp4">`;
+  const videoDiv = createTag('div', { class: 'video-content' });
+  const videoElement = document.createElement('video');
+  videoElement.innerHTML = `<source src="${videoLink.href}" type="video/mp4">`;
 
-    videoElement.muted = true;
-    videoElement.autoplay = true;
-    videoElement.loop = true;
-    videoElement.playsinline = true;
-    videoElement.poster = posterImage;
+  videoElement.muted = true;
+  videoElement.autoplay = true;
+  videoElement.loop = true;
+  videoElement.playsinline = true;
+  videoElement.poster = posterImage;
 
-    videoDiv.appendChild(videoElement);
-    block.append(videoDiv);
-    divToReplace.remove();
+  videoDiv.appendChild(videoElement);
+  block.append(videoDiv);
 
-    videoElement.play();
-  });
+  videoElement.play();
 }
