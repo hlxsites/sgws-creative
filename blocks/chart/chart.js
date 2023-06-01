bc63a4a6a802e9fec4b3726dbd5eff68696d8863
Tania Mathern <mathern@adobe.com>
2023-06-01 13:04:31 -0700

    Read config for known list of fields

--- blocks/chart/chart.js
+++ blocks/chart/chart.js
@@ -1,3 +1,20 @@
-export default function decorate(block) {
+import { readPredefinedBlockConfig } from '../../scripts/lib-franklin.js';
 
+export default function decorate(block) {
+  const readOptions = {
+    configFields: [
+                    'value-suffix',
+                    'unit',
+                    'title',
+                    'chart-scale',
+                    'chart-scale-step',
+                    'scale-step-suffix'
+                  ],
+    removeAfterRead: true,
+  };
+  const cfg = readPredefinedBlockConfig(block, readOptions);
+
+  console.log("Read block config ~~~~~~~~~~~~~~~~");
+  console.log(cfg);
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 }
--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -294,6 +294,57 @@ export function readBlockConfig(block) {
 }
 
 /**
+ * Extracts the config from a block.
+ * @param {Element} block The block element
+ * @returns {object} The block config
+ */
+export function readPredefinedBlockConfig(block, readOptions) {
+  const config = {};
+  block.querySelectorAll(':scope > div').forEach((row) => {
+    if (row.children) {
+      const cols = [...row.children];
+      if (cols[1]) {
+        const col = cols[1];
+        const name = toClassName(cols[0].textContent);
+
+        if(!readOptions?.configFields?.includes(name)) return;
+        console.log(`Read config for ${name}`);
+
+        let value = '';
+        if (col.querySelector('a')) {
+          const as = [...col.querySelectorAll('a')];
+          if (as.length === 1) {
+            value = as[0].href;
+          } else {
+            value = as.map((a) => a.href);
+          }
+        } else if (col.querySelector('img')) {
+          const imgs = [...col.querySelectorAll('img')];
+          if (imgs.length === 1) {
+            value = imgs[0].src;
+          } else {
+            value = imgs.map((img) => img.src);
+          }
+        } else if (col.querySelector('p')) {
+          const ps = [...col.querySelectorAll('p')];
+          if (ps.length === 1) {
+            value = ps[0].textContent;
+          } else {
+            value = ps.map((p) => p.textContent);
+          }
+        } else value = row.children[1].textContent;
+        config[name] = value;
+      }
+
+      if(readOptions?.removeAfterRead){
+        row.remove();
+      }
+    }
+  });
+  return config;
+}
+
+/**
  * Decorates all sections in a container element.
  * @param {Element} main The container element
  */
