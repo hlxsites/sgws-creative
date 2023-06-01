1397b03a16fcc603af04f450515f34f201089ded
Tania Mathern <mathern@adobe.com>
2023-06-01 15:44:59 -0700

    Look at classes to define kind of chart

--- blocks/chart/chart.js
+++ blocks/chart/chart.js
@@ -7,7 +7,7 @@ function readBlockData(block){
     if(row.children.length !== 2) return;
     data[index] = {
       name: row.children[0].innerText,
-      value: row.children[1].innerText
+      value: row.children[1].innerText,
     };
     row.remove();
   });
@@ -19,11 +19,13 @@ export default function decorate(block) {
   const readOptions = {
     configFields: [
       'value-suffix',
+      'value-prefix',
       'unit',
       'title',
       'chart-scale',
       'chart-scale-step',
       'scale-step-suffix',
+      'scale-step-prefix',
     ],
     removeAfterRead: true,
   };
@@ -35,4 +37,12 @@ export default function decorate(block) {
   console.log("Read block data ~~~~~~~~~~~~~~~~");
   console.log(data);
   console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+  const blockClassList = block.classList;
+
+  if(blockClassList.contains('bars')){
+    console.log("Build bar chart");
+  } else if (blockClassList.contains('pie')){
+    console.log("Build pie chart");
+  }
 }
