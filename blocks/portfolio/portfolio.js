aeaa1cc644045a7c2e32ebffcca3320bd0c2ecf2
Anthony Rumsey <arumsey@adobe.com>
2023-06-02 15:10:26 -0400

    feat: Portfolio block #10
    - load portfolio JSON
    - create a grid based off of portfolio data
    - add styling to grid
    - absolute image placement

--- blocks/portfolio/portfolio.js
+++ blocks/portfolio/portfolio.js
@@ -1,5 +1,5 @@
 /* portfolio block */
-import { getMetadata, readBlockConfig } from '../../scripts/lib-franklin.js';
+import { getMetadata, readBlockConfig, createOptimizedPicture } from '../../scripts/lib-franklin.js';
 import { createTag } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
@@ -15,10 +15,8 @@ export default async function decorate(block) {
     return;
   }
 
-  const json = await resp.json();
-  console.log(json);
-
   block.textContent = '';
+  const json = await resp.json();
 
   Object.entries(portfolioConfig).forEach(([sheet, title]) => {
     if (!json[sheet]) {
@@ -34,12 +32,24 @@ export default async function decorate(block) {
     data.forEach((product) => {
       const card = createTag('li');
       card.innerHTML = `<a href="${product.url}" target="_blank">
-        <p class="picture"></p>
+        <p class="picture picture-1"/>
+        <p class="picture picture-2"/>
+        <p class="picture picture-3"/>
         <p>${product.name}</p>
         <p class="variant">${product.variant}</p>
       </a>`;
+      // add images
+      card.querySelectorAll('p.picture').forEach((parent, i) => {
+        const imageUrl = product[`image${i + 1}`];
+        if (!imageUrl) {
+          return;
+        }
+        const picture = createOptimizedPicture(imageUrl, product.name, false, [{ width: 64 }]);
+        parent.append(picture);
+      });
       grid.append(card);
     });
+
     block.append(grid);
   });
 }
