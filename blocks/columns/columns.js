0d9985bde4e48040c4b0205e50dd2dd6bcfde126
Andrew Top <top@adobe.com>
2023-06-05 17:30:29 -0400

    Icons layout #27 - CSS selectors a little different, allow 'icon-list' and 'columns-img-col' to coexist (peacefully)

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -5,19 +5,20 @@ export default function decorate(block) {
   const iconCols = block.querySelectorAll(':scope > div > div > *:first-child:has(span.icon)');
   if (iconCols.length === cols.length) {
     block.classList.add('icon-list');
-  } else {
-    // setup image columns
-    [...block.children].forEach((row) => {
-      [...row.children].forEach((col) => {
-        const pic = col.querySelector('picture');
-        if (pic) {
-          const picWrapper = pic.closest('div');
-          if (picWrapper && picWrapper.children.length === 1) {
-            // picture is only content in column
-            picWrapper.classList.add('columns-img-col');
-          }
-        }
-      });
-    });
   }
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
+
 }
