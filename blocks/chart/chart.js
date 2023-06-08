f2cf1bd3ce5e793e4dae49ffdfc89e00e22155ef
Tania Mathern <mathern@adobe.com>
2023-06-08 11:39:00 -0700

    Small refactor

--- blocks/chart/chart.js
+++ blocks/chart/chart.js
@@ -533,38 +533,6 @@ function drawComparisonPieChart(chartData, chartConfig, chartHolder, theme) {
   console.log("~~~~~~~~~ ")
 
   const pieChartSpecificDescription = {
-    title: {
-      text: 'Nightingale Chart',
-      subtext: 'Fake Data',
-      left: 'center'
-    },
-    tooltip: {
-      trigger: 'item',
-      formatter: '{a} <br/>{b} : {c} ({d}%)'
-    },
-    legend: {
-      left: 'center',
-      top: 'bottom',
-      data: [
-        'rose1',
-        'rose2',
-        'rose3',
-        'rose4',
-        'rose5',
-        'rose6',
-        'rose7',
-        'rose8'
-      ]
-    },
-    toolbox: {
-      show: true,
-      feature: {
-        mark: { show: true },
-        dataView: { show: true, readOnly: false },
-        restore: { show: true },
-        saveAsImage: { show: true }
-      }
-    },
     series: [
       {
         name: 'Radius Mode',
