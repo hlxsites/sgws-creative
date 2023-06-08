35a03ad777988031de0995ec5acaefc163f35d95
Tania Mathern <mathern@adobe.com>
2023-06-08 13:59:27 -0700

    Prepare pie chart

--- blocks/chart/chart.js
+++ blocks/chart/chart.js
@@ -535,7 +535,7 @@ function drawComparisonPieChart(chartData, chartConfig, chartHolder, theme) {
   const pieChartSpecificDescription = {
     series: [
       {
-        name: 'Radius Mode',
+        name: chartConfig.title,
         type: 'pie',
         radius: [20, 140],
         center: ['25%', '50%'],
@@ -552,34 +552,20 @@ function drawComparisonPieChart(chartData, chartConfig, chartHolder, theme) {
           }
         },
         data: [
-          { value: 40, name: 'rose 1' },
-          { value: 33, name: 'rose 2' },
-          { value: 28, name: 'rose 3' },
-          { value: 22, name: 'rose 4' },
-          { value: 20, name: 'rose 5' },
-          { value: 15, name: 'rose 6' },
-          { value: 12, name: 'rose 7' },
-          { value: 10, name: 'rose 8' }
+          formattedData.dataValues[0],
         ]
       },
       {
-        name: 'Area Mode',
+        name: chartConfig.title,
         type: 'pie',
-        radius: [20, 140],
+        radius: [20, 70],
         center: ['75%', '50%'],
         roseType: 'area',
         itemStyle: {
           borderRadius: 5
         },
         data: [
-          { value: 30, name: 'rose 1' },
-          { value: 28, name: 'rose 2' },
-          { value: 26, name: 'rose 3' },
-          { value: 24, name: 'rose 4' },
-          { value: 22, name: 'rose 5' },
-          { value: 20, name: 'rose 6' },
-          { value: 18, name: 'rose 7' },
-          { value: 16, name: 'rose 8' }
+          formattedData.dataValues[1],
         ]
       }
     ]
