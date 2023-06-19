e70d6fe01e367c2e2b2482122e87642175159e23
Tania Mathern <mathern@adobe.com>
2023-06-19 13:17:46 -0700

    Refactor

--- blocks/billboard/billboard.js
+++ blocks/billboard/billboard.js
@@ -1,16 +1,5 @@
 import { getTheme, hasTheme, createTag } from '../../scripts/scripts.js';
 
-/**
- * Structure:
- * Background images:
- * - First row (0) is the "deepest" background image
- * - Last row (4) is second deepest background image (optional)
- * *
- * Content:
- * - Second row (1) is logo + logo decorations left and right
- * - Third row (2) is the main text + optional decorations left and right
- * - Fourth row (3) is products left, products right and middle poster text (optional)
- */
 export default function decorate(block) {
   const themeName = [...block.closest('.section').classList].find((className) => hasTheme(className));
   getTheme(themeName).forEach(({ token, value }) => {
@@ -18,10 +7,6 @@ export default function decorate(block) {
   });
 
   const blockChildren = [...block.children];
-  const logo = blockChildren[1];
-  const mainContent = blockChildren[2];
-  const productContent = blockChildren[3];
-
   block.innerHTML = '';
 
   const backgroundStyleImage = blockChildren[0].firstElementChild.querySelector('img');
@@ -30,13 +15,9 @@ export default function decorate(block) {
   /* bottom background image */
   const topBackgroundImage = blockChildren[4];
   topBackgroundImage.querySelector('img').loading = 'eager';
-
   topBackgroundImage.classList.add('backdrop-image');
-  logo.classList.add('logo-row-layout');
-  mainContent.classList.add('main-row-layout');
-  productContent.classList.add('products-row-layout');
 
   const contentHolder = createTag('div', { class: 'content-holder' });
-  contentHolder.append(logo, mainContent, productContent, topBackgroundImage);
+  contentHolder.append(blockChildren[1], blockChildren[2], blockChildren[3], topBackgroundImage);
   block.append(contentHolder);
 }
