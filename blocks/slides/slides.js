afd0143646118e52e1a76acf2316827e9a88b9f2
Andrew Top <top@adobe.com>
2023-06-20 15:02:04 -0400

    Slides Block: Product overlay #52

--- blocks/slides/slides.js
+++ blocks/slides/slides.js
@@ -1,4 +1,4 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+import { createOptimizedPicture, decorateIcons } from '../../scripts/lib-franklin.js';
 import { createTag, animationObserver } from '../../scripts/scripts.js';
 
 function getSelectedSlide(block) {
@@ -47,7 +47,65 @@ export default function decorate(block) {
     });
     slide.querySelectorAll('p.picture').forEach((p) => p.remove());
     slide.prepend(avatar);
+
+    /* Set up 'pairs with' view, if there are two headers provided. */
+    const headers = slide.querySelectorAll('h3');
+    if (headers.length === 2) {
+      const middleDiv = slide.querySelectorAll('div')[1];
+      const mainHeader = middleDiv.querySelector('h3');
+      if (mainHeader) {
+        const toggle = createTag('div', { class: 'toggle-pairs-with animate' });
+        toggle.innerHTML = `<button type="button" aria-label="Showcase hover">
+              <span class="icon icon-pairs-circle"></span>
+              <span class="icon icon-pairs-covered"></span>
+              <span class="icon icon-pairs-glass"></span>
+          </button>`;
+        mainHeader.parentNode.insertBefore(toggle, mainHeader.nextElementSibling);
+
+        const pairedButton = toggle.querySelector('button');
+        pairedButton.addEventListener('click', () => {
+          slide.classList.toggle('pairs');
+        });
+      }
+
+      const pairsWith = createTag('div', {
+        class: 'pairs-with animate',
+        role: 'group',
+      });
+      const pairsImages = createTag('div', { class: 'pairs-with-meal animate' });
+
+      const meal = slide.querySelector('picture:last-child');
+      pairsImages.append(meal);
+      const bottle = avatar.querySelector('picture:last-child').cloneNode(true);
+      pairsImages.append(bottle);
+      pairsWith.append(pairsImages);
+
+      const pairsText = createTag('div', { class: 'pairs-with-text animate' });
+      const header1 = createTag('h3', { class: 'animate' });
+      header1.innerText = 'A PERFECT PAIRING:';
+      headers[1].innerText = `${headers[0].innerText} + ${headers[1].innerText}`;
+      pairsText.append(header1);
+      pairsText.append(headers[1]);
+
+      const description = slide.querySelector('p:last-child');
+      pairsText.append(description);
+      pairsWith.append(pairsText);
+
+      const closePairsView = createTag('div', { class: 'close-pairs' });
+      closePairsView.innerHTML = `<button type="button" aria-label="Close view">
+              <span class="icon icon-close"></span>
+              </button>`;
+      const closeButton = closePairsView.querySelector('button');
+      closeButton.addEventListener('click', () => {
+        slide.classList.toggle('pairs');
+      });
+      pairsWith.append(closePairsView);
+
+      slide.append(pairsWith);
+    }
   });
+
+  decorateIcons(block);
 
   if (block.children.length > 1) {
     // add navigation buttons
