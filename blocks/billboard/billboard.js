818910b7aed082d94f321696c3bfba6368f09ee1
tmathern <60901087+tmathern@users.noreply.github.com>
2023-06-20 12:57:38 -0700

    Issue 78: First program layout slide (#83)
    * Images
    
    * Bottom backdrop
    
    * Bottom backdrop initial layer
    
    * Add content
    
    * Initial flex layout
    
    * Initial flex layout with bottom images fixed-height
    
    * Set background
    
    * Middle row layouting
    
    * Middle row layouting
    
    * Logo row layouting
    
    * Layout changes
    
    * Layout changes
    
    * Scale images
    
    * Bottle placement
    
    * Handle all fonts
    
    * Clean up
    
    * Endless scaling
    
    * Images layout
    
    * Borders for logo
    
    * Refactor
    
    * Images position
    
    * Layouting
    
    * Outline for box
    
    * Layouting all done
    
    * Layouting all done
    
    * Refactor
    
    * Load theme
    
    * Get theme from section
    
    * Get theme from section
    
    * Make linter happy
    
    * Refactor
    
    * Refactor
    
    * Spacing
    
    * Fix aspect ratio
    
    * Add missing swap
    
    * Fix display
    
    * Legible font sizes
    
    * Handle footer
    
    * Background image is important and loads eagerly
    
    * Using theming for fonts
    
    * Lint
    
    * Change around things
    
    * Min sizing
    
    * Min sizing
    
    * Other image
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * LCP blocks
    
    * LCP blocks
    
    * LCP blocks
    
    * Remove autos
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Add amrgins
    
    * Remove fonts folder
    
    * optimize images
    
    * optimize images
    
    * optimize images
    
    * Image sizing
    
    * Lint
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Layout
    
    * Add video
    
    * Optimize poster picture
    
    * Image sizing
    
    * Image sizing
    
    * Improve video hero
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Fix layout
    
    * Fix footer aspect ratio
    
    * Fix footer aspect ratio
    
    * Add animations

---
+++ blocks/billboard/billboard.js
@@ -0,0 +1,30 @@
+import { getTheme, hasTheme, createTag } from '../../scripts/scripts.js';
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+  const blockChildren = [...block.children];
+  /* block background image */
+  const backgroundStyleImage = blockChildren[0].firstElementChild.querySelector('img');
+
+  block.innerHTML = '';
+  block.style.backgroundImage = `url(${backgroundStyleImage.src})`;
+
+  /* bottom background image */
+  const topBackgroundImage = blockChildren[4];
+  topBackgroundImage.querySelector('img').loading = 'eager';
+
+  topBackgroundImage.classList.add('backdrop-image');
+  blockChildren[1].classList.add('logo-row-layout');
+  blockChildren[2].classList.add('main-row-layout');
+  blockChildren[3].classList.add('products-row-layout');
+
+  const contentHolder = createTag('div', { class: 'content-holder' });
+  blockChildren[3].querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '190' }])));
+  contentHolder.append(blockChildren[1], blockChildren[2], blockChildren[3], topBackgroundImage);
+  block.append(contentHolder);
+
+  const themeName = [...block.closest('.section').classList].find((className) => hasTheme(className));
+  getTheme(themeName).forEach(({ token, value }) => {
+    block.style.setProperty(`--${token}`, `${value}`);
+  });
+}
