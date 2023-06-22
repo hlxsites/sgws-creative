d978e8113d411b46765cd4b052924d3aa1c4012c
Tania Mathern <mathern@adobe.com>
2023-06-22 10:50:31 -0700

    Force use of webp for poster

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -43,6 +43,7 @@ export default function decorate(block) {
   videoElement.playsinline = false;
   videoElement.preload = 'metadata';
   if (image && image.src) {
+    // force use of webp for posters
     let newUrl = image.src.replace('\.png', '\.webp');
     newUrl = newUrl.replace('format=png', 'format=webp');
     videoElement.poster = newUrl;
