c645984f59b5dd1da388f76cb099cb92e7031cdb
Tania Mathern <mathern@adobe.com>
2023-06-23 13:47:34 -0700

    Initial improved DOM

--- blocks/poster/poster.js
+++ blocks/poster/poster.js
@@ -1,24 +1,30 @@
 import { getTheme, hasTheme, createTag } from '../../scripts/scripts.js';
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   const blockChildren = [...block.children];
 
-  const backgroundStyleImage = blockChildren[0];
+  const stylingImages = blockChildren[0];
+  const backgroundImage = stylingImages.children[0].querySelector('img');
+  const posterImage = stylingImages.children[1];
+  block.style.backgroundImage = `url(${backgroundImage.src})`;
+  stylingImages.remove();
+
   const logo = blockChildren[1];
   const title = blockChildren[2];
   const teaser = blockChildren[3];
   const productRow = blockChildren[4];
 
-  backgroundStyleImage.classList.add('background-image');
   logo.classList.add('logo-row-layout');
   title.classList.add('title-row-layout');
   teaser.classList.add('teaser-row-layout');
   productRow.classList.add('product-row-layout');
 
+  const imageHolder = createTag('div', { class: 'poster-image-holder' });
   const contentHolder = createTag('div', { class: 'content-holder' });
   contentHolder.append(logo, title, teaser, productRow);
-  block.append(backgroundStyleImage, contentHolder);
+
+  imageHolder.append(posterImage);
+  block.append(contentHolder, imageHolder);
 
   const themeName = [...block.closest('.section').classList].find((className) => hasTheme(className));
   getTheme(themeName).forEach(({ token, value }) => {
