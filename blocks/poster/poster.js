409c5645449b404169097c4d1e1c36ff46080fa2
tmathern <60901087+tmathern@users.noreply.github.com>
2023-06-26 14:06:27 -0700

    Issue 79: Second program slide (#102)
    * Initial commit
    
    * Initial DOM
    
    * Initial improved DOM
    
    * Flexing design
    
    * Flexing layout
    
    * Images
    
    * Layout
    
    * Styling
    
    * More layout
    
    * More layout
    
    * More layout
    
    * More layout
    
    * More layout
    
    * More layout
    
    * WIP
    
    * Fix icons heihgt issue
    
    * Handle margins
    
    * WIP
    
    * Make linter happy
    
    * Make linter happy
    
    * Fix images
    
    * Fix resize timeout
    
    * Fix tabview bug to display programs
    
    * Fix tabview bug to display programs
    
    * Video optimization
    
    * Reduce theming work
    
    * Make linter happy
    
    * Make linter happier
    
    * Debug logged error
    
    * Handle having no path
    
    * Debug
    
    * Debug
    
    * Defer loading of program slides
    
    * Revert code
    
    * Make linter happy

---
+++ blocks/poster/poster.js
@@ -0,0 +1,43 @@
+import { createTag } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const blockChildren = [...block.children];
+
+  const stylingImages = blockChildren[0];
+  const backgroundImage = stylingImages.children[0].querySelector('img');
+  const posterImage = stylingImages.children[1];
+  block.style.backgroundImage = `url(${backgroundImage.src})`;
+  stylingImages.remove();
+
+  const logo = blockChildren[1];
+  const title = blockChildren[2];
+  const teaser = blockChildren[3];
+  const productRow = blockChildren[4];
+
+  logo.classList.add('logo-row-layout');
+  title.classList.add('title-row-layout');
+  teaser.classList.add('teaser-row-layout');
+  productRow.classList.add('product-row-layout');
+
+  const productsListHolder = createTag('div', { class: 'products-list-holder' });
+  const imageHolder = createTag('div', { class: 'poster-image-holder' });
+  const contentHolder = createTag('div', { class: 'content-holder' });
+  const textContent = productRow.querySelectorAll('p');
+  const productsListMarkers = block.querySelectorAll('h4');
+  const productsListMarkersArray = [...productsListMarkers];
+  const productsListMarker = productsListMarkersArray[productsListMarkersArray.length - 1];
+  productsListHolder.append(...textContent);
+  productsListMarker.closest('div').append(productsListHolder);
+  contentHolder.append(logo, title, teaser, productRow);
+  imageHolder.append(posterImage);
+  block.append(contentHolder, imageHolder);
+
+  /*
+  // Uncomment for debugging CSS with the block alone in a draft
+  const themeName = [...block.closest('.section').classList].find(
+    (className) => hasTheme(className));
+  getTheme(themeName).forEach(({ token, value }) => {
+    block.style.setProperty(`--${token}`, `${value}`);
+  });
+  // */
+}
