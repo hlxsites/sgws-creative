72db09e0fb79b6c0cce24a49c0e515181867416c
Allen Liu <alliu@adobe.com>
2023-06-27 15:47:59 -0700

    fix light house score

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,6 +11,7 @@ import {
   loadBlocks,
   loadFooter,
   loadCSS,
+  createOptimizedPicture,
 } from './lib-franklin.js';
 
 const LCP_BLOCKS = ['video']; // add your LCP blocks to the list
@@ -359,7 +360,8 @@ export function decorateSectionBackgrounds(main) {
     if (backgroundPicture) {
       section.classList.add('background-image');
       const pictureParent = backgroundPicture.closest('p.picture');
-      section.append(backgroundPicture);
+      const image = backgroundPicture.querySelector('img');
+      section.append(createOptimizedPicture(image.src, image.alt, true));
       pictureParent.remove();
     }
   });
