018648676a6cc3454f11e48f0f3cf7653f565617
Andrew Top <top@adobe.com>
2023-06-29 13:35:12 -0400

    Logo-106 - limit the stats background image

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -6,6 +6,20 @@ import {
 } from '../../scripts/scripts.js';
 import { createOptimizedPicture, decorateIcons } from '../../scripts/lib-franklin.js';
 
+function processCellPicture(cell, imgClassName) {
+  const pic = cell.querySelector('picture');
+  if (pic) {
+    const picWrapper = pic.closest('div');
+    if (picWrapper && picWrapper.children.length === 1) {
+      // picture is only content in column
+      picWrapper.classList.add('columns-img-col');
+      if (imgClassName) {
+        picWrapper.classList.add(imgClassName.trim());
+      }
+    }
+  }
+}
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -13,6 +27,26 @@ export default function decorate(block) {
   const iconCols = block.querySelectorAll(':scope > div > div > *:first-child:has(span.icon)');
   if (iconCols.length === cols.length) {
     block.classList.add('icon-list');
+  }
+
+  // Process Banner (single) row (linkable logos, etc.)
+  if (block.classList.contains('banner')) {
+    const firstRow = block.querySelector(':first-child');
+    const bannerCols = [...firstRow.children];
+    if (bannerCols.length === 2) {
+      processCellPicture(bannerCols[0], 'animate');
+
+      const linkP = bannerCols[1].querySelector('p:last-child');
+      const bannerLink = linkP.innerText;
+      bannerCols[1].removeChild(linkP);
+
+      firstRow.addEventListener('click', (e) => {
+        e.stopPropagation();
+        window.open(bannerLink, '_columnsLink');
+      });
+    }
+
+    return;
   }
 
   // setup image columns and help button
@@ -28,7 +62,11 @@ export default function decorate(block) {
         };
 
         const video = createVideoTag(videoLink.href, posterImage?.src, attributes);
-        const playButton = createTag('button', { class: 'play-button', type: 'button', 'aria-label': 'Play Video' });
+        const playButton = createTag('button', {
+          class: 'play-button',
+          type: 'button',
+          'aria-label': 'Play Video',
+        });
         const background = createTag('div', { class: 'video-background' });
         playButton.addEventListener('click', () => {
           video.controls = true;
@@ -45,14 +83,8 @@ export default function decorate(block) {
         animationObserver.observe(videoGroup);
         animationObserver.observe(background);
       }
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
+
+      processCellPicture(col);
 
       const helpIcon = col.querySelector(':scope span.icon-help');
       const tabView = document.querySelector('div.tabview');
@@ -98,12 +130,14 @@ export default function decorate(block) {
   });
 
   // stats background image
-  const backgroundPicture = block.querySelector(':scope > div > .columns-img-col');
-  if (backgroundPicture) {
-    const pictureParent = backgroundPicture.parentElement;
-    const img = backgroundPicture.querySelector('img');
-    pictureParent.remove();
-    block.firstElementChild.style.backgroundImage = `url(${img.src})`;
-    block.classList.add('background-image');
+  if (block.classList.contains('stats')) {
+    const backgroundPicture = block.querySelector(':scope > div > .columns-img-col');
+    if (backgroundPicture) {
+      const pictureParent = backgroundPicture.parentElement;
+      const img = backgroundPicture.querySelector('img');
+      pictureParent.remove();
+      block.firstElementChild.style.backgroundImage = `url(${img.src})`;
+      block.classList.add('background-image');
+    }
   }
 }
