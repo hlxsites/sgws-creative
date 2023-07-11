45eba8966946cd574be47c0355ac18a07b0b9ae2
Tania Mathern <mathern@adobe.com>
2023-07-11 16:15:03 -0700

    Use processed video

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -39,9 +39,7 @@ export default function decorate(block) {
 
   const videoDiv = createTag('div', { class: 'video-content' });
   const videoElement = document.createElement('video');
-  setTimeout(() => {
   videoElement.innerHTML = `<source src="${videoLink.href}" type="video/mp4">`;
-  }, 3000);
 
   videoElement.muted = true;
   videoElement.autoplay = false;
