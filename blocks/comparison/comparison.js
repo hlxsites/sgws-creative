62441bdb072652203df6988ac58a2154ca740653
Tania Mathern <mathern@adobe.com>
2023-07-12 16:22:22 -0700

    Reset

--- blocks/comparison/comparison.js
+++ blocks/comparison/comparison.js
@@ -6,20 +6,20 @@ export default async function decorate(block) {
   }
 
   if (block.classList.contains('bottles')) {
-    const blockChildren = [...block.children];
-    const comparisonHolder = createTag('div', { class: 'bottles-comparison-holder' });
-    const mainBottleGrid = createTag('div', { class: 'main-bottle-grid' });
-    const secondBottleGrid = createTag('div', { class: 'secondary-bottle-grid' });
-    mainBottleGrid.append(
-      blockChildren[0].firstElementChild.nextElementSibling,
-      blockChildren[0].firstElementChild,
-    );
-    secondBottleGrid.append(
-      blockChildren[1].firstElementChild,
-      blockChildren[1].firstElementChild.nextElementSibling,
-    );
-    comparisonHolder.append(mainBottleGrid, secondBottleGrid);
-    const placeholder = createTag('div', { class: 'bottles-comparison-placeholder' });
-    block.append(placeholder, comparisonHolder);
+    console.log("Bottles");
+    //const blockChildren = [...block.children];
+    // const comparisonHolder = createTag('div', { class: 'bottles-comparison-holder' });
+    // const mainBottleGrid = createTag('div', { class: 'main-bottle-grid' });
+    // const secondBottleGrid = createTag('div', { class: 'secondary-bottle-grid' });
+    // mainBottleGrid.append(
+    //   blockChildren[0].firstElementChild.nextElementSibling,
+    //   blockChildren[0].firstElementChild,
+    // );
+    // secondBottleGrid.append(
+    //   blockChildren[1].firstElementChild,
+    //   blockChildren[1].firstElementChild.nextElementSibling,
+    // );
+    // comparisonHolder.append(mainBottleGrid, secondBottleGrid);
+    // block.append(comparisonHolder);
   }
 }
