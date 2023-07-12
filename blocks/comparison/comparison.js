20b1bfb21295bc65ba6e4c9e4a4142aa7ecab59e
Tania Mathern <mathern@adobe.com>
2023-07-12 15:47:48 -0700

    Make linter happier

--- blocks/comparison/comparison.js
+++ blocks/comparison/comparison.js
@@ -5,18 +5,20 @@ export default async function decorate(block) {
     block.classList.add('wood-table');
   }
 
-  if(block.classList.contains('bottles')) {
+  if (block.classList.contains('bottles')) {
     const blockChildren = [...block.children];
-    console.log(blockChildren[0].firstElementChild.nextElementSibling);
-
     const comparisonHolder = createTag('div', { class: 'bottles-comparison-holder' });
     const mainBottleGrid = createTag('div', { class: 'main-bottle-grid' });
     const secondBottleGrid = createTag('div', { class: 'secondary-bottle-grid' });
-
-    mainBottleGrid.append(blockChildren[0].firstElementChild.nextElementSibling, blockChildren[0].firstElementChild);
-    secondBottleGrid.append(blockChildren[1].firstElementChild, blockChildren[1].firstElementChild.nextElementSibling);
+    mainBottleGrid.append(
+      blockChildren[0].firstElementChild.nextElementSibling,
+      blockChildren[0].firstElementChild,
+    );
+    secondBottleGrid.append(
+      blockChildren[1].firstElementChild,
+      blockChildren[1].firstElementChild.nextElementSibling,
+    );
     comparisonHolder.append(mainBottleGrid, secondBottleGrid);
-
     const placeholder = createTag('div', { class: 'bottles-comparison-placeholder' });
     block.append(placeholder, comparisonHolder);
   }
