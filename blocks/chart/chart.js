622667c735860771aea6e4ca879e9c9d979504b6
Tania Mathern <mathern@adobe.com>
2023-07-13 14:26:57 -0700

    Differentiate present-future values

--- blocks/chart/chart.js
+++ blocks/chart/chart.js
@@ -307,20 +307,26 @@ function drawHistogramChartWithOverlay(chartData, chartConfig, chartHolder, them
 function drawHistogramTimeline(chartData, chartConfig, chartHolder, theme) {
   const formattedData = prepareChartData(chartData);
   chartConfig['chart-scale-step'] = parseInt(chartConfig['chart-scale-step'], 10);
+  chartConfig['chart-data-ending'] = parseInt(chartConfig['chart-data-ending'], 10);
 
   // stylings
   formattedData.barNames.forEach(function(element, index) {
     this[index] = Number.parseInt(element, 10);
-  }, formattedData.barNames); // use arr as this
+  }, formattedData.barNames);
   let max = Number.NEGATIVE_INFINITY;
   formattedData.dataValues.forEach((datapoint, i) => {
     datapoint.value = Number(datapoint.value);
     max = Math.max(max, datapoint.value);
 
-    // TODO - style depending on if it is present or future timeline
     datapoint.itemStyle = {
       color: getLinearColorGradient(theme[THEME_TOKEN.PRIMARY_COLOR], theme['primary-gradient-color']),
     };
+    if(formattedData.barNames[i] > chartConfig['chart-data-ending']) {
+      console.log("Value is in future: ", formattedData.barNames[i]);
+    } else {
+      console.log("Value is present: ", formattedData.barNames[i]);
+      datapoint.itemStyle.opacity = 0.6;
+    }
   });
   const axisFontStyle = getBarChartAxisFontStyle(theme);
   console.log('===========================')
